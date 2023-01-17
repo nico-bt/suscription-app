2efef5d9df0f755e93fb9958ca08819b6d8f237b
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-01-17 09:08:55 -0300

    Add rateLimit

--- server/index.js
+++ server/index.js
@@ -7,10 +7,17 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get("/", (req, res)=>{
-    res.send("Working...")
+// Limiting number of request to the API
+const rateLimit = require('express-rate-limit')
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
+app.use(limiter)
 
+// Routes
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/subs", require("./routes/subscription"))
 app.use("/api/articles", require("./routes/articles"))
