b85242041f08c71eea44656bcdf963ed9cfc7a0b
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-01-17 02:24:42 -0300

    Dynamic info in Nav depending if there is a user logged in or not

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Container, Navbar, NavItem, NavLink } from "react-bootstrap"
+import { Link, useNavigate } from "react-router-dom"
+import {UserContext} from "../context/UserContext"
 
 function Nav() {
 
-    const handleLogout = ()=> {
-        console.log("logout")
+    const [user, setUser]= useContext(UserContext)
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        localStorage.removeItem("token")
+        setUser({ data: null, error: null, loading: false})
+        navigate("/")
     }
 
   return (
     <Navbar bg="light">
         <Container >
-            <NavItem >Home</NavItem>
-            
-            <NavItem>User xxx</NavItem>
-            
-            <NavItem> 
+            <NavItem >
+                <Link to={"/"} className="nav-link"> Home </Link>
+            </NavItem>
+
+        {user.data?.email &&
+            <>
+            <NavItem>
+                {user.data.email}
+            </NavItem>
+            <NavItem>
                 <NavLink onClick={handleLogout}> Logout </NavLink>
             </NavItem>
-        
+            </>
+        }
         </Container>
     </Navbar>
   )
