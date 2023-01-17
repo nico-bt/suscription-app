f6f0c08763f2050a2baad41ee0bf426d5f6be583
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-01-17 02:14:10 -0300

    Add checkAuth middleware, extracts id from token and attach to: req.user=user.id

--- server/middleware/checkAuth.js
+++ server/middleware/checkAuth.js
@@ -1,6 +1,31 @@
-const checkAuth = (req, res, next) => {
-    console.log("Checking auth...")
-    next()
+const jwt = require("jsonwebtoken")
+const User = require("../models/User")
+require('dotenv').config()
+
+const checkAuth = async (req, res, next) => {
+    let token = req.header("authorization")
+    
+    if(!token) {
+        return res.status(403).json({msg: "Not Authorized"})
+    }
+    
+    // Get rid of "Bearer"
+    token = token.split(" ")[1]
+    
+    try {
+        const user = await jwt.verify(token, process.env.JWT_SECRET)
+        
+        if(!user){
+            return res.status(403).json({msg: "Not Authorized"})
+        }
+        
+        // Add the id to the request
+        req.user = user.id
+        next()
+        
+    } catch (error) {
+        return res.status(403).json({msg: "Not Authorized"})
+    }
 }
 
 module.exports = checkAuth
