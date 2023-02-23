dd10a7b12995f142f056162d7d424667091ae7e3
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-02-23 00:17:21 -0300

    Change(fix) base url to render: https://articles-suscription.onrender.com

--- server/routes/subscription.js
+++ server/routes/subscription.js
@@ -1,44 +1,45 @@
-const express = require("express")
-const User = require("../models/User")
-const checkAuth = require("../middleware/checkAuth")
-const Stripe = require("stripe")
+const express = require("express");
+const User = require("../models/User");
+const checkAuth = require("../middleware/checkAuth");
+const Stripe = require("stripe");
 
-const stripe = new Stripe(process.env.STRIPE_CLAVE_SECRETA, {apiVersion: "2022-11-15"})
+const stripe = new Stripe(process.env.STRIPE_CLAVE_SECRETA, {
+  apiVersion: "2022-11-15",
+});
 
-const router = express.Router()
+const router = express.Router();
 
 // Base route api/subs
 // -------------------------------------------------------------
-router.get("/prices", checkAuth, async (req,res) => {
-    const prices = await stripe.prices.list({ 
-        apiKey: process.env.STRIPE_CLAVE_SECRETA 
-    })
-    res.json({prices})
-})
-
-router.post("/session", checkAuth, async (req, res) => {
-    try {
-        const user = await User.findById(req.user);
-      
-        const session = await stripe.checkout.sessions.create(
-            {
-                mode: "subscription",
-                payment_method_types: ["card"],
-                line_items: [{ price: req.body.priceId, quantity: 1 },],
-                success_url: "http://suscription-app-production.up.railway.app/articles",
-                cancel_url: "http://suscription-app-production.up.railway.app/articles-plan",
-                customer: user.customerStripeId
-            },
-            {
-                apiKey: process.env.STRIPE_CLAVE_SECRETA,
-            }
-        );
-
-        return res.json(session);
-
-    } catch (error) {
-        res.json(error)
-    }
+router.get("/prices", checkAuth, async (req, res) => {
+  const prices = await stripe.prices.list({
+    apiKey: process.env.STRIPE_CLAVE_SECRETA,
+  });
+  res.json({ prices });
 });
 
-module.exports = router
+router.post("/session", checkAuth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user);
+
+    const session = await stripe.checkout.sessions.create(
+      {
+        mode: "subscription",
+        payment_method_types: ["card"],
+        line_items: [{ price: req.body.priceId, quantity: 1 }],
+        success_url: "https://articles-suscription.onrender.com/articles",
+        cancel_url: "https://articles-suscription.onrender.com/articles-plan",
+        customer: user.customerStripeId,
+      },
+      {
+        apiKey: process.env.STRIPE_CLAVE_SECRETA,
+      }
+    );
+
+    return res.json(session);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+module.exports = router;
