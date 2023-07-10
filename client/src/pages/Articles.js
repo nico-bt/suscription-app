1ed315e5421a7c09db529a66a6c5f5a78fb56420
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-10 18:20:54 -0300

    Show navbar in articles page for logged users

--- client/src/pages/Articles.js
+++ client/src/pages/Articles.js
@@ -1,9 +1,10 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Card, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import ClickeableCard from "../components/ClickeableCard";
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Card, Container } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+import ClickeableCard from "../components/ClickeableCard"
+import Nav from "../components/Nav"
 
 const CardsContainer = styled.div`
   display: grid;
@@ -11,7 +12,7 @@ const CardsContainer = styled.div`
   gap: 1rem;
   margin-top: 2rem;
   margin-bottom: 5rem;
-`;
+`
 
 const NoArticlesContainer = styled.div`
   display: flex;
@@ -28,76 +29,78 @@ const NoArticlesContainer = styled.div`
     padding: 0.2rem 2rem;
     margin-top: 1rem;
   }
-`;
+`
 
 const ErrorHeader = styled.h2`
   font-size: 2.5rem;
-`;
+`
 
 function Articles() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(false)
+  const [articles, setArticles] = useState([])
 
   const getArticles = async () => {
-    setIsLoading(true);
+    setIsLoading(true)
     try {
-      const { data: response } = await axios.get(
-        "https://bored-pear-beret.cyclic.app/api/articles"
-      );
+      const { data: response } = await axios.get("https://bored-pear-beret.cyclic.app/api/articles")
       /* console.log(response) */
-      setArticles(response);
-      setIsLoading(false);
+      setArticles(response)
+      setIsLoading(false)
     } catch (err) {
-      console.log(err);
-      setError(true);
-      setIsLoading(false);
+      console.log(err)
+      setError(true)
+      setIsLoading(false)
     }
-  };
+  }
 
   useEffect(() => {
-    getArticles();
-  }, []);
+    getArticles()
+  }, [])
 
   return (
-    <Container>
-      {isLoading ? (
-        <div className="text-center mt-5">
-          <h1 style={{ color: "white" }}>Loading...</h1>
-        </div>
-      ) : (
-        <CardsContainer>
-          {articles.length &&
-            articles.map((item) => (
-              <ClickeableCard item={item} key={item._id}>
-                <Card.Img variant="top" src={item.imageUrl} />
+    <>
+      <Nav />
 
-                <Card.Body className="d-flex align-items-center justify-content-center">
-                  <Card.Title>{item.title}</Card.Title>
-                </Card.Body>
-              </ClickeableCard>
-            ))}
-        </CardsContainer>
-      )}
+      <Container>
+        {isLoading ? (
+          <div className="text-center mt-5">
+            <h1 style={{ color: "white" }}>Loading...</h1>
+          </div>
+        ) : (
+          <CardsContainer>
+            {articles.length &&
+              articles.map((item) => (
+                <ClickeableCard item={item} key={item._id}>
+                  <Card.Img variant="top" src={item.imageUrl} />
 
-      {!error && !isLoading && !articles.length && (
-        <NoArticlesContainer>
-          <ErrorHeader>You don't have access yet</ErrorHeader>
-          <Link to={"/articles-plan"}>Buy a plan</Link>
-        </NoArticlesContainer>
-      )}
+                  <Card.Body className="d-flex align-items-center justify-content-center">
+                    <Card.Title>{item.title}</Card.Title>
+                  </Card.Body>
+                </ClickeableCard>
+              ))}
+          </CardsContainer>
+        )}
 
-      {error && (
-        <NoArticlesContainer>
-          <ErrorHeader>
-            <div>Sorry...</div>
-            <div>Something went wrong.</div>
-            <div>Please try again later</div>
-          </ErrorHeader>
-        </NoArticlesContainer>
-      )}
-    </Container>
-  );
+        {!error && !isLoading && !articles.length && (
+          <NoArticlesContainer>
+            <ErrorHeader>You don't have access yet</ErrorHeader>
+            <Link to={"/articles-plan"}>Buy a plan</Link>
+          </NoArticlesContainer>
+        )}
+
+        {error && (
+          <NoArticlesContainer>
+            <ErrorHeader>
+              <div>Sorry...</div>
+              <div>Something went wrong.</div>
+              <div>Please try again later</div>
+            </ErrorHeader>
+          </NoArticlesContainer>
+        )}
+      </Container>
+    </>
+  )
 }
 
-export default Articles;
+export default Articles
