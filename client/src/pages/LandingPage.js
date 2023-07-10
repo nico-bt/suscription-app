8d9e8b870aa9b6cddcf65cea255dc6242c611b77
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-10 18:21:24 -0300

    Show loading status

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,24 +1,31 @@
-import React, { useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Hero from "../components/Hero"
-import { UserContext } from "../context/UserContext"
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Hero from "../components/Hero";
+import { UserContext } from "../context/UserContext";
 
 function LandingPage() {
-  const navigate = useNavigate()
-  const [user] = useContext(UserContext)
+  const navigate = useNavigate();
+  const [user] = useContext(UserContext);
 
-  useEffect(()=>{
-    if(user.data) {
-      navigate("/articles")
+  useEffect(() => {
+    if (user.data) {
+      navigate("/articles");
     }
-  },[user, navigate])
+  }, [user, navigate]);
 
-  
-  if(!user.data){
-    return <Hero />
+  if (user.loading) {
+    return (
+      <div className="text-center mt-5">
+        <h1 style={{ color: "white" }}>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (!user.loading && !user.data) {
+    return <Hero />;
   } else {
-    return <div></div>
+    return <div></div>;
   }
 }
 
-export default LandingPage
+export default LandingPage;
