cebc68de4e82903c52e1338187260ba8646f269f
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-10 18:18:31 -0300

    Remove navbar for all routes, only show on articles page when logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,28 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Nav from './components/Nav';
-import LandingPage from './pages/LandingPage';
-import Articles from './pages/Articles';
-import ProtectedRoute from './components/routes/ProtectedRoute';
-import ArticlesPlan from './pages/ArticlePlan';
+import "./App.css"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import LandingPage from "./pages/LandingPage"
+import Articles from "./pages/Articles"
+import ProtectedRoute from "./components/routes/ProtectedRoute"
+import ArticlesPlan from "./pages/ArticlePlan"
 
 function App() {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        
-        <Route path='/articles' element={<ProtectedRoute />}> 
-          <Route path='/articles' element={<Articles />} />
-        </Route>
-        
-        <Route path='/articles-plan' element={<ProtectedRoute />}> 
-          <Route path='/articles-plan' element={<ArticlesPlan />} />
+        <Route path="/" element={<LandingPage />} />
+
+        <Route path="/articles" element={<ProtectedRoute />}>
+          <Route path="/articles" element={<Articles />} />
         </Route>
 
-        <Route path='*' element={ <Navigate to={"/"} />} />
-    </Routes>
-  </BrowserRouter>
-  );
+        <Route path="/articles-plan" element={<ProtectedRoute />}>
+          <Route path="/articles-plan" element={<ArticlesPlan />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
